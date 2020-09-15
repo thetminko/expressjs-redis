9722495112ccdb114ab39d980246e0a38252d767
minko <minko.thet@ufinity.com>
2020-09-15 14:08:59 +0800

    Added redis and express-session

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,39 @@
 import express from 'express';
+import session from 'express-session';
+import Redis from 'redis';
+import connectRedis from 'connect-redis';
+
+const redisStore = connectRedis(session);
+const redisClient = Redis.createClient();
 
 // Create a new express application instance
 const app: express.Application = express();
 
+app.use(session({
+  secret: 'AbC$D',
+  store: new redisStore({ host: 'localhost', client: redisClient, ttl: 100 }),
+  cookie: {
+    httpOnly: true,
+    // secure: 'auto',
+    sameSite: 'strict'
+  },
+  unset: 'destroy',
+  saveUninitialized: false,
+  resave: false
+}));
+
 app.get('/', async (req, res) => {
-  res.json({ msg: 'Hello World!' });
+  const sessionUser = req.session.user ? JSON.parse(req.session.user).email : null;
+  if (!sessionUser) {
+    res.clearCookie('connect.sid');
+    req.session = null;
+  }
+  res.json({ msg: `Hello World!, session: [${sessionUser}]` });
+});
+
+app.get('/add', (req, res) => {
+  req.session.user = JSON.stringify({ userId: 1, name: 'test', email: 'something@gmail.com' });
+  res.json({ msg: 'done' });
 });
 
 app.listen(3000, () => {
